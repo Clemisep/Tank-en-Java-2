7ca4ba9471c5929b17fd929e1a7914fb341d7d8e
Clemisep <ubuwusenno-9596@yopmail.com>
2015-05-13 14:41:34 +0200

    Ajout des éléments principaux de la structure

--- src/Principal/PanneauJeu.java
+++ src/Principal/PanneauJeu.java
@@ -1,5 +1,12 @@
 package Principal;
 
-public class PanneauJeu {
+import java.awt.Dimension;
 
+public class PanneauJeu extends PanneauBase {
+	private static final long serialVersionUID = 1L;
+
+	public PanneauJeu(Dimension taille, Tank[] tanks, Sol sol) {
+		super(taille);
+		
+	}
 }
