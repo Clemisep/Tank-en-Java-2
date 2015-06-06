a7b2084a6e6276468b3f98659b7e3bd12650d5f0
Clemisep <ubuwusenno-9596@yopmail.com>
2015-06-06 20:32:44 +0200

    Gestion correcte de l'emplacement en hauteur des tanks.

--- src/Principal/PanneauMenu.java
+++ src/Principal/PanneauMenu.java
@@ -10,11 +10,13 @@ import java.awt.Graphics;
 
 
 
+import java.io.IOException;
+
 import Automatitem.ClickListener;
 import Automatitem.ItemGroup;
 import Automatitem.ItemText;
 import Automatitem.RepaintListener;
-import Tanks.PetitTank;
+import Tanks.Tank1;
 import Tanks.Tank;
 
 public class PanneauMenu extends PanneauBase implements RepaintListener {
@@ -40,29 +42,37 @@ public class PanneauMenu extends PanneauBase implements RepaintListener {
 		sol = new Sol(taille);
 		equipe1 = new Equipe(1);
 		equipe2 = new Equipe(1);
-		Tank[] tanks = {new PetitTank(equipe1, new Position(30, 200), 100), new PetitTank(equipe2, new Position(800, 250), 100)};
-		this.tanks = tanks;
 		
-		ItemText itemJouer = new ItemText(0, 100, 0, 30, "Jouer");
-		itemJouer.changeClickListener(new ClickListener() {
-
-			@Override
-			public void click() {
-				fen.remplacerPar(new PanneauJeu(fen, taille, tanks, sol));
-			}
+		try {
+			Tank[] tanks = {new Tank1(sol, equipe1, 40, 100), new Tank1(sol, equipe2, 800, 100)};
 			
-		});
-		
-		itemGroup.addItem(itemJouer);
-		
-		String textes[] = {"Jouer", "Paramètres", "Quitter"};
-		
-		for(int i=1 ; i<textes.length ; i++)
-			itemGroup.addItem(new ItemText(0, 100, 40*i, 40*i+30, textes[i]));
-		
-		itemGroup.setCoords(30, 20);
-		
-		itemGroup.changeRepaintListener(this);
+			this.tanks = tanks;
+			
+			ItemText itemJouer = new ItemText(0, 100, 0, 30, "Jouer");
+			itemJouer.changeClickListener(new ClickListener() {
+	
+				@Override
+				public void click() {
+					fen.remplacerPar(new PanneauJeu(fen, taille, tanks, sol));
+				}
+				
+			});
+			
+			itemGroup.addItem(itemJouer);
+			
+			String textes[] = {"Jouer", "Paramètres", "Quitter"};
+			
+			for(int i=1 ; i<textes.length ; i++)
+				itemGroup.addItem(new ItemText(0, 100, 40*i, 40*i+30, textes[i]));
+			
+			itemGroup.setCoords(30, 20);
+			
+			itemGroup.changeRepaintListener(this);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
