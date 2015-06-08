942160c0bdd4af56a97f23c16fe3c7b5094a3593
Clemisep <ubuwusenno-9596@yopmail.com>
2015-06-08 22:58:19 +0200

    Meilleur aspect visuel.

--- src/Principal/PanneauMenu.java
+++ src/Principal/PanneauMenu.java
@@ -3,12 +3,20 @@ package Principal;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 
+
+
+
+
+import javax.imageio.ImageIO;
+
 import Automatitem.ClickListener;
 import Automatitem.ItemGroup;
-import Automatitem.ItemText;
+import Automatitem.ItemImage;
 import Automatitem.RepaintListener;
 import Missiles.Armes;
 import Missiles.Canon;
@@ -21,7 +29,7 @@ import Tanks.TankBasic;
 public class PanneauMenu extends PanneauBase implements RepaintListener {
 	
 	private static final long serialVersionUID = 1L;
-	
+	private final static Color couleurFond = new Color(255, 174, 54);
 	private final int ecartTanks = 70;
 	
 	private ItemGroup itemGroup;
@@ -54,7 +62,8 @@ public class PanneauMenu extends PanneauBase implements RepaintListener {
 			
 			Tank tank2 = new TankBasic(sol, equipe2, getWidth() - ecartTanks, 1, this, armes2);
 			
-			ItemText itemJouer1 = new ItemText(0, 100, 0, 30, "Un tank par joueur");
+			BufferedImage imageJouer1 = ImageIO.read(new File("src/Images/unTank.png"));
+			ItemImage itemJouer1 = new ItemImage(-imageJouer1.getWidth(), 0, imageJouer1);
 			itemJouer1.changeClickListener(new ClickListener() {
 				
 				@Override
@@ -69,7 +78,8 @@ public class PanneauMenu extends PanneauBase implements RepaintListener {
 			});
 			itemGroup.addItem(itemJouer1);
 			
-			ItemText itemJouer2 = new ItemText(0, 100, 40, 70, "Deux tanks par joueur");
+			BufferedImage imageJouer2 = ImageIO.read(new File("src/Images/deuxTanks.png"));
+			ItemImage itemJouer2 = new ItemImage(-imageJouer2.getWidth(), imageJouer1.getHeight()+30, imageJouer2);
 			itemJouer2.changeClickListener(new ClickListener() {
 
 				@Override
@@ -113,7 +123,7 @@ public class PanneauMenu extends PanneauBase implements RepaintListener {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.BLUE);
+		g.setColor(couleurFond);
 		g.fillRect(0, 0, getWidth(), getHeight());
 		
 		itemGroup.paintComponent(g);
