a574297b18ee2eb85436cb92381bfaac2ca5bac8
Clemisep <ubuwusenno-9596@yopmail.com>
2015-06-08 23:58:36 +0200

    Améliorations.

--- src/Principal/PanneauMenu.java
+++ src/Principal/PanneauMenu.java
@@ -6,12 +6,6 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
-
-
-
-
-
 import javax.imageio.ImageIO;
 
 import Automatitem.ClickListener;
